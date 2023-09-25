23e7f1d6149ba04bbf947adf4b2bae3231a0630f
moses <moses.kingi@moringaschool.com>
2023-09-26 02:02:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+const countDown = ["0","1","2","3","4","5","6","7","8","9","10","11"];
+
+function numbers(countDown ) {
+    for (let countDown = 0; countDown <10; countDown++) {
+        console.log(countDown);
+    debugger;
+  }
+
+  return countDown;
+}
+
+numbers(countDown);
